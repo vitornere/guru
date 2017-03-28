e3803223e0bc88fe604b934b2827e4f2b84875d3
Vitor Nere <vitor.nere@hotmail.com>
2017-03-28 15:33:59 -0300

    corrigido caminho dos arquivos staticos

--- guru/settings.py
+++ guru/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../guru_responde/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #WAGTAIL_SITE_NAME = "Guru"
